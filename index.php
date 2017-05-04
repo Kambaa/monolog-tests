74b6c50edba0eab2323dd62f5106eb80f11bd999
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2017-05-04 17:22:43 +0300

    init

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yg
+ * Date: 5/4/17
+ * Time: 4:51 PM
+ */
+
+require_once "vendor/autoload.php";
+use Monolog\Logger;
+
+$logger = new Logger("default");
+
+/**
+ * {
+ * "ok": true,
+ * "access_token": "xoxp-178796779717-178135058545-179510000118-369aa3e39408ff35ec06f4a712eda81c",
+ * "scope": "identify,bot,chat:write:bot",
+ * "user_id": "U583Z1QG1",
+ * "team_name": "ExtreApp",
+ * "team_id": "T58PENXM3"
+ * }
+ *
+ * https://github.com/Seldaek/monolog/issues/743
+ */
+$slackHandler = new \Monolog\Handler\SlackHandler('xoxp-178796779717-178135058545-179510000118-369aa3e39408ff35ec06f4a712eda81c', '#general', 'SonsuratExtreAppLog');
+
+$logger->pushHandler($slackHandler);
+$slackHandler->setLevel(\Monolog\Logger::INFO);
+
+$logger->info("Hello from monolog with slack integration!");
+
+
